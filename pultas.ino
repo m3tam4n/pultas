a7fa3c8717fae6e2bea77aabd38c6e7fde2458c6
m3tam4n <savickaspovke@gmail.com>
2016-03-02 10:20:16 +0200

    Control-panel 1.1
    Program used for the control panel of tamping machine.

--- pultas.ino
+++ pultas.ino
@@ -1,4 +1,4 @@
-
+#include <MemoryFree.h>
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -15,17 +15,6 @@ int adc_key_in  = 0;
 int read_LCD_buttons()
 {
   adc_key_in = analogRead(0);      // read the value from the sensor
-  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
-  // we add approx 50 to those values and check to see if we are close
-  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
-  // For V1.1 us this threshold
-  /*
-    if (adc_key_in < 50)   return btnRIGHT;
-    if (adc_key_in < 250)  return btnUP;
-    if (adc_key_in < 450)  return btnDOWN;
-    if (adc_key_in < 650)  return btnLEFT;
-    if (adc_key_in < 850)  return btnSELECT;
-  */
   // For V1.0 comment the other threshold and use the one below:
   if (adc_key_in < 50)   return btnRIGHT;
   if (adc_key_in < 195)  return btnUP;
@@ -37,30 +26,24 @@ int read_LCD_buttons()
   return btnNONE;  // when all others fail, return this...
 }
 
-const int sensorTpin = 22;  //virsaus sensorius
-const int sensorBLpin = 30; //apatines kaires padeties sensorius
-const int sensorBRpin = 26; //apatines desines padeties sensorius
-const int sensorLpin = 34; //kaires puses sensorius
-const int sensorRpin = 38; //desines puses sensorius
-const int MovVpin = 42; //vertikalaus cilindro valdymas
-const int MovHpin = 46; //horizontalaus cilindro valdymas
-const int Butpin = 53; //pedalas
+const int sensorTpin = 22;  //Top sensor
+const int sensorBLpin = 30; //Bottom left sensor
+const int sensorBRpin = 26; //Bottor right sensor
+const int sensorLpin = 34; //Left end sensor
+const int sensorRpin = 38; //Right end sensor
+const int MovVpin = 42; //Vertical movement control
+const int MovHpin = 46; //Horizontal movement control
+const int Butpin = 53; //Pedal
 
-int mode = 4;//mygtuku pradines pozicijos
+int mode = 4;//default mode
 int modeA = 4;
-int but = 0;
-int ped = 0;
-int sensT = 0;
-int sensBL = 0;
-int sensBR = 0;
-int sensL = 0;
-int sensR = 0;
 
-void setup() {  
 
+void setup() {
+ Serial.begin(115200);
   lcd.begin(16, 2);              // start the library
   lcd.setCursor(0, 0);
-  lcd.print("Rezimas"); // print a simple message
+  lcd.print("Rezimas"); // Translation 'mode'
 
   pinMode(MovVpin, OUTPUT);
   pinMode(MovHpin, OUTPUT);
@@ -70,12 +53,13 @@ void setup() {
   pinMode(sensorLpin, INPUT);
   pinMode(sensorRpin, INPUT);
   pinMode(Butpin, INPUT);
-  // put your setup code here, to run once:
   digitalWrite(MovHpin, LOW);
   digitalWrite(MovVpin, HIGH);
 }
 
 void loop() {
+  //Serial.println(str);
+ // Serial.println("freeMemory()=");
   lcd.setCursor(0, 1);           // move to the begining of the second line
   lcd_key = read_LCD_buttons();  // read the buttons
 
@@ -101,13 +85,13 @@ void loop() {
         }
         break;
       }
-    case btnSELECT:
+    case btnSELECT: // confirm mode
       {
         modeA = mode;
         break;
       }
   }
-  switch (mode) {
+  switch (mode) { //Selecting mode
     case 1:
       {
         lcd.print("Klise      ");
@@ -134,19 +118,18 @@ void loop() {
       }
   }
   switch (modeA) {
-    case 1:
+    case 1: // Vertical movement in the left position
       {
         digitalWrite(MovHpin, LOW);
         if (digitalRead(Butpin) == HIGH) {
           while (digitalRead(sensorBLpin) != HIGH) {
             digitalWrite(MovVpin, LOW);
-            sensT = digitalRead(sensorTpin);
           }
           digitalWrite(MovVpin, HIGH);
         }
         break;
       }
-    case 2:
+    case 2: // Vertical movement in the right position
       {
         digitalWrite(MovHpin, HIGH);
         digitalWrite(MovVpin, HIGH);
@@ -159,22 +142,23 @@ void loop() {
         }
         break;
       }
-    case 3:
+    case 3: //Horizontal movement
       {
         digitalWrite(MovHpin, HIGH);
         digitalWrite(MovVpin, HIGH);
-        while (digitalRead(Butpin) == HIGH) {
-          ;
-          if (digitalRead(sensorLpin) == HIGH) {
-            digitalWrite(MovHpin, HIGH);
-          }
-          if (digitalRead(sensorRpin) == HIGH) {
+        if (digitalRead(Butpin) == HIGH) {
+          while (digitalRead(sensorLpin) != HIGH) {
             digitalWrite(MovHpin, LOW);
           }
+            delay(200);
+          while (digitalRead(sensorRpin) != HIGH) {
+            digitalWrite(MovHpin, HIGH+);
+          }
+          delay(200);
         }
         break;
       }
-    case 4:
+    case 4: //Full working cycle
       {
         digitalWrite(MovHpin, HIGH);
         digitalWrite(MovVpin, HIGH);
@@ -182,23 +166,27 @@ void loop() {
           while (digitalRead(sensorLpin) != HIGH) { //1 zingsnis
             digitalWrite(MovHpin, LOW);
           }
+          delay(200);
           while (digitalRead(sensorBLpin) != HIGH) { //2 zingsins
             digitalWrite(MovVpin, LOW);
           }
           //3 zingsnis
           digitalWrite(MovVpin, HIGH);
+          delay(400);
           while (digitalRead(sensorRpin) != HIGH) { //4 zingsnis
             digitalWrite(MovHpin, HIGH);
           }
+          delay(200);
           while (digitalRead(sensorBRpin) != HIGH) { //5 zingsnis
             digitalWrite(MovVpin, LOW);
-          } //6 zingsnis
+          }//6 zingsnis
           digitalWrite(MovVpin, HIGH);
-
+          delay(200);
         }
         break;
       }
   }
 }
-// put your main code here, to run repeatedly:
+
+
 
